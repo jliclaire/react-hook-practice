281797a3ac8e773e527f9e506f1190a07d777759
jingli <jingli.claire@gmail.com>
2020-01-12 16:18:55 +1100

    useState Hook

---
+++ src/README.md
@@ -0,0 +1,29 @@
+# Notes
+
+## React Hooks Rules
+
+- only use in functional components or inside other custom React Hooks
+- must use on the root level of the function component, cannot be nested it in another function or in an if statement
+
+## useState
+
+- `useState` can take any state (type of data), not just object. class components only take object.
+- `useSate` returns
+
+`[{ title: string; amount: string; }, React.Dispatch<React.SetStateAction<{ title: string; amount: string; }]`
+
+- unless need to data together, otherwise, manage states independently, split states into multiple states, prefer not to use object, so they don't have to be kept track with updates of other states.
+
+if initialState is `null` or `undefined`, don't pass anything, `useState()`
+
+e.g.
+
+`const[title, setTitle] = useState('') const[amount, setAmount] = useState('')`
+
+instead of
+
+`const [inputState, setInputState] = useState({ title: "", amount: "" });`
+
+## array destructing
+
+- pull element out of an array and store them in separate variables
--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
+  // method ONE
+  // const inputState = useState({
+  //   title: "",
+  //   amount: ""
+  // });
+
+  //method TWO, array destructing
+  // array destructing, here:
+  // [{ title: string; amount: string; }, React.Dispatch<React.SetStateAction<{ title: string; amount: string; }]
+  // inputState = inputState[0] (here is { title: string; amount: string; })
+  // setInputState = inputState[1], the update data function (here is: React.Dispatch<React.SetStateAction<{ title: string; amount: string; })
+  // const [inputState, setInputState] = useState({
+  //   title: "",
+  //   amount: ""
+  // });
+
+  //method THREE, manage states independently
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +35,67 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              // method ONE
+              // value={inputState[0].title}
+              // onChange={event => {
+              //   const newTitle = event.target.value;
+              //   inputState[1](prevInputState => ({
+              //     title: newTitle,
+              //     amount: prevInputState.amount
+              //   }));
+              // }}
+
+              //method TWO, array destructing
+              // value={inputState.title}
+              // onChange={event => {
+              //   const newTitle = event.target.value;
+              //   setInputState(prevInputState => ({
+              //     title: newTitle,
+              //     amount: prevInputState.amount
+              //   }));
+              // }}
+
+              //method THREE, manage states independently
+              value={enteredTitle}
+              onChange={event => {
+                setEnteredTitle(event.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              // method ONE
+              // value={inputState[0].amount}
+              // onChange={event => {
+              //   const newAmount = event.target.value;
+              //   inputState[1](prevInputState => ({
+              //     amount: newAmount,
+              //     title: prevInputState.title
+              //   }));
+              // }}
+
+              //method TWO, array destructing
+              // value={inputState.amount}
+              // onChange={event => {
+              //   const newAmount = event.target.value;
+              //   setInputState(prevInputState => ({
+              //     amount: newAmount,
+              //     title: prevInputState.title
+              //   }));
+              // }}
+
+              //method THREE, manage states independently
+              value={enteredAmount}
+              onChange={event => {
+                setEnteredAmount(event.target.value);
+              }}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
